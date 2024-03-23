c83e745cd50689d1d504b0e6aab69834ac5484dd
ArtemisSaber <20559407+ArtemisSaber@users.noreply.github.com>
2024-03-23 16:38:38 +0800

    init project

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import { createServer } from 'node:http'
+import { WebSocketServer } from 'ws'
+import { io } from 'socket.io-client'
+
+const app = express()
+const server = createServer(app)
+const webSocketServer = new WebSocketServer({ server })
+const clientSocket = io('http://localhost:3001')
+
+app.get('/', (req, res) => {
+    return res.send('Api working')
+})
+
+webSocketServer.on('connection', (ws) => {
+    let timeout = setTimeout(() => {
+        ws.isAlive = false
+    }, 10000)
+    ws.on('open', () => {
+        console.log('connected ws')
+    })
+    ws.on('error', console.warn)
+    clientSocket.onAny((event, ...args) => {
+        const eventData = {
+            event,
+            args
+        }
+        ws.send(JSON.stringify(eventData))
+    })
+    ws.on('message', (data) => {
+        clearTimeout(timeout)
+        clientSocket.send(data)
+        timeout = setTimeout(() => {
+            ws.isAlve = false
+        }, 10000)
+    })
+})
+
+server.listen(8000, () => {
+    console.log('server running at port 8000')
+})
